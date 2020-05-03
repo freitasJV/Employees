812015d042d0fe0fc122273a07fc526891a308eb
freitasJV <jose.freitas16@fatec.sp.gov.br>
2020-05-03 15:50:25 -0300

    Adicionando tratamento de excessões

--- Employees/Program.cs
+++ Employees/Program.cs
@@ -9,40 +9,51 @@ namespace Employees
     {
         static void Main(string[] args)
         {
-            List<Employee> list = new List<Employee>();
-
-            Console.Write("Digite o número de funcionários: ");
-            int n = int.Parse(Console.ReadLine());
-
-            for (int i = 1; i <= n; i++)
+            try
             {
-                Console.WriteLine($"Dados do #{i} Funcionário:");
-                Console.Write("Terceiro (s/n)? ");
-                char terceiro = char.Parse(Console.ReadLine());
-                Console.Write("Nome: ");
-                string name = Console.ReadLine();
-                Console.Write("Horas: ");
-                int hours = int.Parse(Console.ReadLine());
-                Console.Write("Valor por hora: ");
-                double valuePerHour = double.Parse(Console.ReadLine());
+                List<Employee> list = new List<Employee>();
 
-                if (terceiro.ToString().ToUpper().Equals("S"))
+                Console.Write("Digite o número de funcionários: ");
+                int n = int.Parse(Console.ReadLine());
+
+                for (int i = 1; i <= n; i++)
                 {
-                    Console.Write("Despesas Adicionais: ");
-                    double additionalCharge = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-                    list.Add(new OutsourcedEmployee(name, hours, valuePerHour, additionalCharge));
+                    Console.WriteLine($"Dados do #{i} Funcionário:");
+                    Console.Write("Terceiro (s/n)? ");
+                    char terceiro = char.Parse(Console.ReadLine());
+                    Console.Write("Nome: ");
+                    string name = Console.ReadLine();
+                    Console.Write("Horas: ");
+                    int hours = int.Parse(Console.ReadLine());
+                    Console.Write("Valor por hora: ");
+                    double valuePerHour = double.Parse(Console.ReadLine());
+
+                    if (terceiro.ToString().ToUpper().Equals("S"))
+                    {
+                        Console.Write("Despesas Adicionais: ");
+                        double additionalCharge = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                        list.Add(new OutsourcedEmployee(name, hours, valuePerHour, additionalCharge));
+                    }
+                    else
+                    {
+                        list.Add(new Employee(name, hours, valuePerHour));
+                    }
                 }
-                else
+                Console.WriteLine();
+                Console.WriteLine("Pagamentos: ");
+
+                foreach (Employee emp in list)
                 {
-                    list.Add(new Employee(name, hours, valuePerHour));
+                    Console.WriteLine($"{emp.Name} - R$ {emp.Payment().ToString("F2", CultureInfo.InvariantCulture)}");
                 }
             }
-            Console.WriteLine();
-            Console.WriteLine("Pagamentos: ");
-
-            foreach (Employee emp in list)
+            catch (FormatException e)
             {
-                Console.WriteLine($"{emp.Name} - R$ {emp.Payment().ToString("F2", CultureInfo.InvariantCulture)}");
+                Console.WriteLine($"Erro de formatação: {e.Message}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Erro inesperado: {e.Message}");
             }
         }
     }
